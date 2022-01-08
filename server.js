ac16b0e36244de50e8c23f9cbd7182e3979155aa
tzbodula <tzbodula@gmail.com>
2022-01-08 17:55:12 -0500

    Database Connection

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
 const express = require('express')
+const mysql = require("mysql")
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'zqfarrmy_thomas',
+    password: '3Tzc7J#dW*Oy',
+    database: 'zqfarrmy_users'
+})
+
+db.connect( (error) => {
+  if(error) {
+      console.log(error)
+  } else {
+      console.log("MySQL Connected...")
+  }
+})
+
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
